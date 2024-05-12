9dcdba5295fe9337571a0057c2a65418459cb348
CyberNet <serg.karetnuk333@gmail.com>
2024-05-12 22:49:02 +0200

    add env + fix

--- settings.py
+++ settings.py
@@ -14,3 +14,16 @@ DB_URL = env.str(
     "DB_URL",
     default=f"postgresql+asyncpg://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_DATABASE}",
 )
+
+
+DB_HOST_TEST = env.str("DB_HOST_TEST", default="0.0.0.0")
+DB_PORT_TEST = env.int("DB_PORT_TEST", default="5432")
+
+DB_DATABASE_TEST = env.str("DB_DATABASE_TEST", default="postgres_test")
+DB_USERNAME_TEST = env.str("DB_USERNAME_TEST", default="postgres_test")
+DB_PASSWORD_TEST = env.str("DB_PASSWORD_TEST", default="postgres_test")
+
+DB_URL_TEST = env.str(
+    "DB_URL_TEST",
+    default=f"postgresql+asyncpg://{DB_USERNAME_TEST}:{DB_PASSWORD_TEST}@{DB_HOST_TEST}:{DB_PORT_TEST}/{DB_DATABASE_TEST}",
+)
